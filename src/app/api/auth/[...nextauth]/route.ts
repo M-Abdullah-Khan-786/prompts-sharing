83d76556a6df94cebb4384ca2310385ff5ab4259
Muhammad Abdullah Khan <abdullahkhanlodhi1458@gmail.com>
2025-04-02 03:22:57 +0500

    Create User Model & Auth API Routes

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,24 @@
 import { connectDB } from "@/utils/database";
-import NextAuth, { AuthOptions } from "next-auth";
-import type { Profile, Account, User } from "next-auth";
+import NextAuth, { AuthOptions, Session } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import User from "@/models/user.model";
+
+interface Profile {
+  email?: string;
+  name?: string;
+  picture?: string;
+}
+
+declare module "next-auth" {
+  interface Session {
+    user: {
+      id: string;
+      name?: string | null;
+      email?: string | null;
+      image?: string | null;
+    };
+  }
+}
 
 const authOptions: AuthOptions = {
   providers: [
@@ -11,12 +28,34 @@ const authOptions: AuthOptions = {
     }),
   ],
   callbacks: {
-    async session({ session }) {
+    async session({ session }: { session: Session }) {
+      if (!session?.user || !session.user.email) {
+        return session;
+      }
+
+      const sessionUser = await User.findOne({ email: session.user.email });
+
+      if (sessionUser) {
+        session.user.id = sessionUser._id.toString();
+      }
+
       return session;
     },
-    async signIn({ user, account, profile }: { user: User; account: Account | null; profile?: Profile }) {
+    async signIn({ profile }: { profile?: Profile }) {
       try {
         await connectDB();
+        const userExist = await User.findOne({
+          email: profile?.email,
+        });
+
+        if (!userExist) {
+          await User.create({
+            email: profile?.email,
+            username: profile?.name?.replace(" ", "").toLowerCase(),
+            image: profile?.picture,
+          });
+        }
+
         return true;
       } catch (error) {
         console.error(error);
