c7a246596b851bdc9fbecc89a430ea092526abb9
Muhammad Abdullah Khan <abdullahkhanlodhi1458@gmail.com>
2025-04-10 19:43:57 +0500

    Integrate Get All Prompt API in Feed Component

--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -1,8 +1,49 @@
-import React from 'react'
+'use client'
+import { useEffect, useState } from "react"
+import PromptCardList from "./PromptCardList"
 
 const Feed = () => {
+  const [searchText, setSearchText] = useState("")
+  const [prompts, setPrompts] = useState([])
+
+  const handleSearch = (e:any) => {
+    setSearchText(e.target.value)
+  }
+
+  const fetchPosts = async ()=>{
+    try {
+      const response = await fetch('/api/prompt')
+      const data = await response.json()
+      setPrompts(data)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  const handleTagClick = (tag: string) => {
+    console.log(tag)
+  }
+
+  useEffect(() => {
+    fetchPosts()
+  }, [])
+  
+
   return (
-    <div>Feed</div>
+    <section className="feed">
+      <form className="relative w-full flex-center">
+        <input type="text" placeholder="Search for a tag or username"
+        value={searchText}
+        onChange={handleSearch}
+        required
+        className="search_input peer"
+        />
+      </form>
+      <PromptCardList
+      data={prompts}
+      handleTagClick = {handleTagClick}
+      />
+    </section>
   )
 }
 
