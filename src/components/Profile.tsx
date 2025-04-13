c08ee0240bfe68179da4e8fa8d0b978014a2daa9
Muhammad Abdullah Khan <abdullahkhanlodhi1458@gmail.com>
2025-04-13 12:44:37 +0500

    Create the Profile Component and Passing the Prompt Data & Actions Function in PromptCard Component

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,9 +1,43 @@
-import React from 'react'
-
-const Profile = () => {
-  return (
-    <div>Profile</div>
-  )
+import React from "react";
+import PromptCard from "./PromptCard";
+interface Prompt {
+  _id: string;
+  prompt: string;
+  tag: string;
 }
 
-export default Profile
+interface ProfileProps {
+  name: string;
+  desc: string;
+  data: Prompt[];
+  handleEdit: (prompt: Prompt) => void;
+  handleDelete: (prompt: Prompt) => void;
+}
+const Profile: React.FC<ProfileProps> = ({
+  name,
+  desc,
+  data,
+  handleEdit,
+  handleDelete,
+}) => {
+  return (
+    <section className="w-full">
+      <h1 className="head_text text-left">
+        <span className="blue_gradient">{name} Profile</span>
+      </h1>
+      <p className="desc text-left">{desc}</p>
+      <div className="mt-10 prompt_layout">
+        {data.map((prompt) => (
+          <PromptCard
+            key={prompt._id}
+            prompt={prompt}
+            handleEdit={() => handleEdit && handleEdit(prompt)}
+            handleDelete={() => handleDelete && handleDelete(prompt)}
+          />
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default Profile;
